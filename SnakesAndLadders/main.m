5e3f9616b173f04659108cf6e791a89b2f1d6456
Andrew Shopland <hi@ashp.land>
2017-10-06 22:31:11 -0700

    Initial Commit

---
+++ SnakesAndLadders/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SnakesAndLadders
+//
+//  Created by Andrew on 2017-10-06.
+//  Copyright © 2017 Andrew. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
